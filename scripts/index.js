4e2151201fdad7fdbc1b9805d4006810f7e66310
Henrique-T <hfoesm@hotmail.com>
2022-07-20 19:13:28 -0300

    Add proper interaction with real contract

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,13 @@ async function main() {
     const accounts = await ethers.provider.listAccounts();
     console.log("***accounts****")
     console.log(accounts);
+
+    const address = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512';
+    const OwnerToCompany = await ethers.getContractFactory('OwnerToCompany');
+    const ownerToCompany = await OwnerToCompany.attach(address);
+
+    const value = await ownerToCompany.getName();
+    console.log('ownerToCompany value is', value.toString());
   }
   
   main()
