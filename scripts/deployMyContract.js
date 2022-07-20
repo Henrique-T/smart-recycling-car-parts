b2f25b6d153569d8f2eb254fb06808429ec157ba
Henrique-T <hfoesm@hotmail.com>
2022-07-20 18:15:18 -0300

    Add deploy to owner-to-company

--- scripts/deployMyContract.js
+++ scripts/deployMyContract.js
@@ -1,8 +1,12 @@
 async function main() {
     const MyContract = await ethers.getContractFactory("MyContract");
     const myContract = await MyContract.deploy("My Contract");
+
+    const OwnerToCompany = await ethers.getContractFactory("OwnerToCompany");
+    const ownerToCompany = await OwnerToCompany.deploy("owner to company");
   
-    console.log("My Contract deployed to:", myContract.address);
+    console.log("My Contract deployed to address:", myContract.address);
+    console.log("Owner-to-company deployed to address:", ownerToCompany.address);
   }
   
   main()
